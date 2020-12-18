f12bb0f59c693bdf7d4ff2197271e2602855f5f3
Tabatha Bell <tabathambell@live.com>
2020-12-17 21:15:24 -0500

    inquirer and fs installed and started structures

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,36 @@
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+  .prompt(questions)
+
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+}
 
 // Function call to initialize app
 init();
-
-// test
