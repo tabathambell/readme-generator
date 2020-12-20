055f17c06253e060a36cb8d1a7bdb0eba21737d1
Tabatha Bell <tabathambell@live.com>
2020-12-19 23:58:38 -0500

    debugging

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if('Apache') {
+  if(license==='Apache') {
     return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-  } else if ('Boost') {
+  } else if (license==='Boost') {
     return "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
-  } else if ('BSD') {
+  } else if (license==='BSD') {
     return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
   } else {
     return '';
@@ -15,20 +15,18 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(license) {
-    return "This application is covered under " + (data.license)
-  } else if (''){
-    return '';
-  }
+if (license) {
+return 'This application is covered under ' + license + ' ! ';
+}
 };
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-  # ${data.title}
-  ${renderLicenseBadge(data.license)}
-  ## Description of Application 
-    ${data.description}
+# ${data.title}
+${renderLicenseBadge(data.license)}
+## Description of Application 
+${data.description}
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
@@ -36,17 +34,16 @@ function generateMarkdown(data) {
 * [License](#license)
 * [Testing](#test)
 * [Contact](#questions)
-  ## How to Use This Application
-    ${data.usage}
-  ## Contributors to this Application
-    ${data.contributors}
-  ## Licenses 
-    ${data.license}
-    ${renderLicenseSection(data.license)}
-  ## How to Test This Application
-     ${data.test}
-  ## Do You Have Qusetions?
-     ${data.questions}
+## How to Use This Application
+${data.usage}
+## Contributors to this Application
+${data.contributors}
+## ${data.license}
+${renderLicenseSection(data.license)}
+## How to Test This Application
+${data.test}
+## Do You Have Questions?
+${data.questions}
 `;
 }
 
